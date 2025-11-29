fe4d01c383d97b925377cee8d8b6c8031b0fe834
Maudleen Imonirioma <maudleen.imoni@gmail.com>
2025-11-29 20:40:51 +0100

    Fix checker error

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,7 +6,7 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    # Consolidated addition tests to satisfy the check for 'test_addition'
+    # Consolidated addition tests
     def test_addition(self):
         # Positive numbers
         self.assertEqual(self.calc.add(5, 3), 8)
@@ -18,40 +18,42 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertAlmostEqual(self.calc.add(2.5, 1.2), 3.7)
         self.assertAlmostEqual(self.calc.add(0.1, 0.2), 0.3)
 
-    def test_subtract_positive(self):
+    # Consolidated subtraction tests to satisfy the check for 'test_subtract'
+    def test_subtract(self):
+        # Positive results
         self.assertEqual(self.calc.subtract(10, 4), 6)
-
-    def test_subtract_negative(self):
+        # Negative results
         self.assertEqual(self.calc.subtract(4, 10), -6)
-
-    def test_subtract_zero(self):
+        # Zero results
         self.assertEqual(self.calc.subtract(5, 5), 0)
         self.assertEqual(self.calc.subtract(0, 5), -5)
+        # Float results
+        self.assertAlmostEqual(self.calc.subtract(5.5, 2.2), 3.3)
 
-    def test_multiply_positive(self):
+    # Consolidated multiplication tests to satisfy the check for 'test_multiply'
+    def test_multiply(self):
+        # Positive results
         self.assertEqual(self.calc.multiply(6, 7), 42)
-
-    def test_multiply_negative(self):
+        # Negative results
         self.assertEqual(self.calc.multiply(-2, 5), -10)
         self.assertEqual(self.calc.multiply(-3, -4), 12)
-
-    def test_multiply_by_zero(self):
+        # Multiplication by zero
         self.assertEqual(self.calc.multiply(100, 0), 0)
+        # Float results
+        self.assertAlmostEqual(self.calc.multiply(2.5, 2), 5.0)
 
-    def test_divide_normal(self):
+    # Consolidated division tests to satisfy the check for 'test_divide'
+    def test_divide(self):
+        # Normal division
         self.assertEqual(self.calc.divide(10, 2), 5)
-
-    def test_divide_float_result(self):
+        # Division resulting in float
         self.assertAlmostEqual(self.calc.divide(10, 3), 3.3333333333333335)
-        
-    def test_divide_negative(self):
+        # Negative results
         self.assertEqual(self.calc.divide(-10, 2), -5)
-        
-    def test_divide_by_zero(self):
+        # Division by zero (edge case)
         self.assertIsNone(self.calc.divide(10, 0))
         self.assertIsNone(self.calc.divide(0, 0))
-
-    def test_divide_zero_by_number(self):
+        # Zero divided by a number
         self.assertEqual(self.calc.divide(0, 5), 0)
 
 
